064897fddfff3d2e1dc23312656dfa288bb15aa5
iem <iem@iutabgdin202-07.univ-lyon1.fr>
2017-10-18 15:26:01 +0200

    Connexion avec une API de test (CN Fatcs)

---
+++ app/src/main/java/com/example/iem/pokecards/Async.java
@@ -0,0 +1,55 @@
+package com.example.iem.pokecards;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Async extends AsyncTask<Object, Void, String> {
+    TextView tv;
+    @Override
+    protected String doInBackground(Object... params) {
+
+        URL url = null;
+        HttpURLConnection urlConnection = null;
+        BufferedReader in = null;
+        String heure="3";
+        tv = (TextView) params[0];
+        try {
+            url = new URL("https://api.chucknorris.io/jokes/random");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                in = new BufferedReader(
+                        new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
+                String temp="";
+                while((temp = in.readLine()) != null) {
+                    heure += temp;
+                }
+//ensuite, on récupère le contenu du flux avec in.readLine(), tant qu’il y a des
+//données dans le flux d’entrée
+
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return heure;
+
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        tv.setText(s);
+    }
+}
--- app/src/main/java/com/example/iem/pokecards/MainActivity.java
+++ app/src/main/java/com/example/iem/pokecards/MainActivity.java
@@ -1,13 +1,82 @@
 package com.example.iem.pokecards;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
+    CallbackManager callbackManager;
+    LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //connexionFacebook();
+        Button bouton = (Button) findViewById(R.id.bouton);
+       /* WebView test2 = (WebView) findViewById(R.id.webView);
+        test2.loadUrl("https://api.chucknorris.io/jokes/random");*/
+        bouton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView test = (TextView) findViewById(R.id.tv);
+                new Async().execute(test);
+
+            }
+        });
     }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        }
+
+
+   /* protected void connexionFacebook(){
+        callbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(MainActivity.this, "Succès", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(MainActivity.this, "Cancel", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(MainActivity.this, "Error" + exception, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }*/
 }
